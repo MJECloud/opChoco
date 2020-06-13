46c9b386f349a34585673f7aed421537b8d8a2a2
MJECloud <mjecld@outlook.com>
2020-06-13 17:52:27 +0200

    update to Version 1.1.0

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,12 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $packageName = '1password-cli'
-$version     = 'v1.0.0'
+$version     = 'v1.1.0'
 $toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url         = "https://cache.agilebits.com/dist/1P/op/pkg/$version/op_windows_386_$version.zip"
-$checksum    = 'BEE7F1909297B102DD0111B75FF38EC1F0773BA8E747F05338A35A58F7669528'
+$checksum    = 'DFEFDF712B3B141254BB34B604B98150F264F56BC0A72E5ACDDD98C49ABF8044'
 $url64       = "https://cache.agilebits.com/dist/1P/op/pkg/$version/op_windows_amd64_$version.zip"
-$checksum64  = "486CA9AAC17DB5E9A090B48B02CA448C6EC17B07C47FF3CA242570B09910BE40"
+$checksum64  = "3D76324EA99B33CC3D981984EE2CE7B5CACE96ECEBE618FF40F8A95670DF884F"
 
 $packageArgs = @{
   packageName    = $packageName
